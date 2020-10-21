d939f889ec6b43f034cdcccd0b777ac14357f1d6
Rajeev Paulose <rajeevpoulose@outlook.com>
2020-10-21 21:51:12 +1100

    Added Model project

---
+++ GITHubSpaceGame/SpaceGame.Models/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SpaceGame.Models
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
